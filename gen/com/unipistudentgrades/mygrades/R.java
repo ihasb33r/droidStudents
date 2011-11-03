a42e2dabd3f2cd6d3033586094b50e0f5ccd338e
Kostas Papakonstantinou <bckdotgr@gmail.com>
2011-11-03 17:00:31 +0200

    fixed preferences

--- gen/com/unipistudentgrades/mygrades/R.java
+++ gen/com/unipistudentgrades/mygrades/R.java
@@ -14,18 +14,20 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int course_grade=0x7f070001;
-        public static final int course_name=0x7f070000;
-        public static final int headerview=0x7f070003;
-        public static final int preferences=0x7f070004;
-        public static final int spacerview=0x7f070002;
-        public static final int update=0x7f070005;
+        public static final int course_grade=0x7f070002;
+        public static final int course_name=0x7f070001;
+        public static final int grade_list_item_layout=0x7f070000;
+        public static final int headerlayout=0x7f070003;
+        public static final int headerview=0x7f070005;
+        public static final int preferences=0x7f070006;
+        public static final int spacerview=0x7f070004;
+        public static final int update=0x7f070007;
     }
     public static final class layout {
         public static final int grade_list_item=0x7f030000;
         public static final int header=0x7f030001;
         public static final int main=0x7f030002;
-        public static final int preferences=0x7f030003;
+        public static final int studentspreferences=0x7f030003;
     }
     public static final class menu {
         public static final int unipi_menu=0x7f060000;
--- src/com/unipistudentgrades/mygrades/Preferences.java
+++ src/com/unipistudentgrades/mygrades/Preferences.java
@@ -15,7 +15,7 @@ public class Preferences extends PreferenceActivity
         public void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.layout.preferences);
+            addPreferencesFromResource(R.layout.studentspreferences);
 
         }
 }
